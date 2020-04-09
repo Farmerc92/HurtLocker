75ad16715bd00fc10c905773afe4f4c1c5ac8110
Farmerc92 <farmerc@udel.edu>
2020-04-09 16:15:06 -0400

    reformatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,6 +35,29 @@ public class Main {
         return productsList;
     }
 
+    public static LinkedHashMap<String, LinkedHashMap<String, Integer>> convertToMap(ArrayList<Product> productsList){
+        LinkedHashMap<String, LinkedHashMap<String, Integer>> productMap = new LinkedHashMap<>();
+        for (Product p : productsList) {
+            if (!productMap.containsKey(p.getName())) {
+                addNewKey(productMap, p);
+            } else {
+                addToExistingKey(productMap, p);
+            }
+        }
+        return productMap;
+    }
+
+    public static void addNewKey(LinkedHashMap<String, LinkedHashMap<String, Integer>> productMap, Product p){
+        LinkedHashMap<String, Integer> prices = new LinkedHashMap<>();
+        prices.put(p.getPrice(), 1);
+        productMap.put(p.getName(), prices);
+    }
+
+    public static void addToExistingKey(LinkedHashMap<String, LinkedHashMap<String, Integer>> productMap, Product p){
+        LinkedHashMap<String, Integer> prices = productMap.get(p.getName());
+        prices.put(p.getPrice(), prices.getOrDefault(p.getPrice(), 0) + 1);
+    }
+
     public static void buildOutput(LinkedHashMap<String, LinkedHashMap<String, Integer>> productMap, StringBuilder builder){
         for (String name : productMap.keySet()) {
             LinkedHashMap<String, Integer> priceMap = productMap.get(name);
@@ -43,6 +66,12 @@ public class Main {
             builder.append("\n");
         }
         builder.append(String.format("Errors \t\t\t\t seen: %d times", parser.getErrorCount()));
+    }
+
+    public static void appendNameAndSum(LinkedHashMap<String, Integer> priceMap, String name, StringBuilder builder){
+        Integer sum = priceMap.values().stream().reduce(0, Integer::sum);
+        builder.append(String.format("name:%8s \t\t seen: %d times\n", name, sum));
+        builder.append(String.format("%s \t"+" "+"\t %s\n", equalBreak(), equalBreak()));
     }
 
     public static void buildPriceOutput(LinkedHashMap<String, Integer> priceMap, StringBuilder builder){
@@ -73,51 +102,19 @@ public class Main {
         return dash.toString();
     }
 
-    public static void myOutputTxt(String output){
-        try (PrintStream out = new PrintStream(new FileOutputStream("MyOutput.txt"))) {
-            out.print(output);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static LinkedHashMap<String, LinkedHashMap<String, Integer>> convertToMap(ArrayList<Product> productsList){
-        LinkedHashMap<String, LinkedHashMap<String, Integer>> productMap = new LinkedHashMap<>();
-        for (Product p : productsList) {
-            if (!productMap.containsKey(p.getName())) {
-                addNewKey(productMap, p);
-            } else {
-                addToExistingKey(productMap, p);
-            }
-        }
-        return productMap;
-    }
-
-    public static void addNewKey(LinkedHashMap<String, LinkedHashMap<String, Integer>> productMap, Product p){
-        LinkedHashMap<String, Integer> prices = new LinkedHashMap<>();
-        prices.put(p.getPrice(), 1);
-        productMap.put(p.getName(), prices);
-    }
-
-    public static void addToExistingKey(LinkedHashMap<String, LinkedHashMap<String, Integer>> productMap, Product p){
-        LinkedHashMap<String, Integer> prices = productMap.get(p.getName());
-        if (prices.containsKey(p.getPrice()))
-            prices.put(p.getPrice(), prices.get(p.getPrice()) + 1);
-        else
-            prices.put(p.getPrice(), 1);
-    }
-
-    public static void appendNameAndSum(LinkedHashMap<String, Integer> priceMap, String name, StringBuilder builder){
-        Integer sum = priceMap.values().stream().reduce(0, Integer::sum);
-        builder.append(String.format("name:%8s \t\t seen: %d times\n", name, sum));
-        builder.append(String.format("%s \t"+" "+"\t %s\n", equalBreak(), equalBreak()));
-    }
-
     public static int appendDashAndCheckCount(StringBuilder builder, int dashCount){
         if (dashCount < 1) {
             builder.append(String.format("%s\t\t %s\n", dashBreak(), dashBreak()));
             dashCount++;
         }
         return dashCount;
+    }
+
+    public static void myOutputTxt(String output){
+        try (PrintStream out = new PrintStream(new FileOutputStream("MyOutput.txt"))) {
+            out.print(output);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
